4ad1e920ba7f17038b5db61c015a7e27a7ab3ae5
CNFLKT7 <danilatit@yandex.ru>
2021-11-06 21:50:39 +0300

    Начало проекта!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace NewMessenger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Начало проекта!");
         }
     }
 }
